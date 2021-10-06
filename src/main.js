5e6f5d47f8ed7edeb7812d21acc154c1562325dd
Albert <a.minvaliev@mosproektkompleks.ru>
2021-10-06 16:27:35 +0300

    Реализовал валидацию формы на странице Login.vue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
-import './registerServiceWorker'
+import Vuelidate from "vuelidate";
 import router from './router'
 import store from './store'
+import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
+Vue.use(Vuelidate)
 Vue.use(VueRouter)
 
 Vue.config.productionTip = false
