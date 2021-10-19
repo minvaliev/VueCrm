e2b5239e98785e3bbd61bd23a524c6e30ecb044e
Albert <a.minvaliev@mosproektkompleks.ru>
2021-10-19 16:55:31 +0300

    Реализовал функционал на странице "Планирование"

--- src/store/category.js
+++ src/store/category.js
@@ -6,9 +6,6 @@ export default {
             try {
                 const uid = await dispatch('getUid')
                 const categories = (await firebase.database().ref(`/users/${uid}/categories`).once('value')).val() || {}
-
-                const cats = []
-
                 return Object.keys(categories).map(key => ({...categories[key], id: key}))
 
             } catch (e) {
