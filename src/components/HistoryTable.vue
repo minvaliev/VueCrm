c35c64999f7142d499c26121a122722d2111549d
Albert <a.minvaliev@mosproektkompleks.ru>
2021-11-01 15:56:06 +0300

    Реализовал защиту роутов и вывод таблицу записей на страницу "История"

---
+++ src/components/HistoryTable.vue
@@ -0,0 +1,69 @@
+<template>
+    <table>
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>Сумма</th>
+            <th>Дата</th>
+            <th>Категория</th>
+            <th>Тип</th>
+            <th>Открыть</th>
+        </tr>
+        </thead>
+
+        <tbody>
+        <tr v-for="(record, idx) of records" :key="record.id">
+            <td>{{idx + 1}}</td>
+            <td>{{record.amount | currency('RUB')}}</td>
+            <td>{{dateFilter(record.date, 'date')}}</td>
+            <td>{{record.categoryName}}</td>
+            <td>
+                <span
+                    :class="[record.typeClass]"
+                    class="white-text badge red"
+                    >{{record.typeText}}</span>
+            </td>
+            <td>
+                <button
+                   v-tooltip="'Открыть запись'"
+                    class="btn-small btn"
+                    @click="$router.push('/detail-record/' + idx)"
+                >
+                    <i class="material-icons">open_in_new</i>
+                </button>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+    export default {
+        name: "HistoryTable",
+        props: {
+            records: {
+                required: true,
+                type: Array
+            }
+        },
+        methods: {
+            dateFilter(value, format = 'date') {
+                value = value
+                const options= {}
+                if (format.includes('date')) {
+                    options.day = '2-digit'
+                    options.month = 'long'
+                    options.year = 'numeric'
+                    options.hour = '2-digit'
+                    options.minute = '2-digit'
+                    options.second = '2-digit'
+                }
+                return new Intl.DateTimeFormat('ru-RU', options).format(new Date(value))
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import firebase from "firebase/compat";
 
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    meta: {layout: 'main'},
+    meta: {layout: 'main', auth: true},
     component: () => import('../views/Home')
 
   },
@@ -26,37 +27,37 @@ const routes = [
   {
     path: '/categories',
     name: 'categories',
-    meta: {layout: 'main'},
+    meta: {layout: 'main', auth: true},
     component: () => import('../views/Categories')
   },
   {
     path: '/history',
     name: 'history',
-    meta: {layout: 'main'},
+    meta: {layout: 'main', auth: true},
     component: () => import('../views/History')
   },
   {
     path: '/record',
     name: 'record',
-    meta: {layout: 'main'},
+    meta: {layout: 'main', auth: true},
     component: () => import('../views/Record')
   },
   {
-    path: '/detail-record',
+    path: '/detail-record/:id',
     name: 'detail-record',
-    meta: {layout: 'main'},
+    meta: {layout: 'main', auth: true},
     component: () => import('../views/DetailRecord')
   },
   {
     path: '/planning',
     name: 'planning',
-    meta: {layout: 'main'},
+    meta: {layout: 'main', auth: true},
     component: () => import('../views/Planning')
   },
   {
     path: '/profile',
     name: 'profile',
-    meta: {layout: 'main'},
+    meta: {layout: 'main', auth: true},
     component: () => import('../views/Profile')
   }
 ]
@@ -65,6 +66,18 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser
+  const requireAuth = to.matched.some(record => record.meta.auth)
+
+  if (requireAuth && !currentUser) {
+    next('/login?message=login')
+  } else {
+    next()
+  }
+
 })
 
 export default router
